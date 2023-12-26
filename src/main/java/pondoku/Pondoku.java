638a17035b6db8cf806c211ee516b225a104dc49
fathifathan <fathifathan30@gmail.com>
2023-12-26 14:13:47 +0700

    commit 1

---
+++ src/main/java/pondoku/Pondoku.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pondoku;
+
+/**
+ *
+ * @author HP
+ */
+public class Pondoku {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
